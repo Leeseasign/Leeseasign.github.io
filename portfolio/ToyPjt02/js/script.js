25ecf29e8c51cd4a3966b7db38f5362830dd0b5a
Leeseasign <Leeseasign@gmail.com>
2025-02-26 14:22:46 +0900

    fix: coupangeats nav

--- portfolio/ToyPjt02/js/script.js
+++ portfolio/ToyPjt02/js/script.js
@@ -1,20 +1,39 @@
 $(document).ready(function(){
+  function navActive(){
+    $("nav.navbar").addClass("nav-back2");
+    $("nav.navbar").removeClass("nav-back1");
+    $("nav.navbar-light ul.navbar-nav li:not(:last-child) a").addClass("nav-text2");
+    $("nav.navbar-light ul.navbar-nav li:not(:last-child) a").removeClass("nav-text1");
+    $(".navbar-brand img.logo_img1").addClass("hide");
+    $(".navbar-brand img.logo_img2").removeClass("hide");
+  }
+  function navInit(){
+    $("nav.navbar").removeClass("nav-back2");
+    $("nav.navbar").addClass("nav-back1");
+    $("nav.navbar-light ul.navbar-nav li:not(:last-child) a").addClass("nav-text1");
+    $("nav.navbar-light ul.navbar-nav li:not(:last-child) a").removeClass("nav-text2");
+    $(".navbar-brand img.logo_img1").removeClass("hide");
+    $(".navbar-brand img.logo_img2").addClass("hide");
+  }
+
+  // resize 이벤트
+  let winWidth = $(window).width();
+  $(window).resize(function(){
+    winWidth = $(window).width();
+  });
+
+  // 스크롤 이벤트
   $(window).scroll(function(){
     let scrollPosition = $(window).scrollTop(); // 현재 스크롤바 위치
-    if(scrollPosition > 50){
-      $("nav.navbar").addClass("nav-back2");
-      $("nav.navbar").removeClass("nav-back1");
-      $("nav.navbar-light ul.navbar-nav li:not(:last-child) a").addClass("nav-text2");
-      $("nav.navbar-light ul.navbar-nav li:not(:last-child) a").removeClass("nav-text1");
-      $(".navbar-brand img.logo_img1").addClass("hide");
-      $(".navbar-brand img.logo_img2").removeClass("hide");
+    navActive();
+    if(winWidth > 979){
+      if(scrollPosition > 100){
+        navActive();
+      }else{
+        navInit();
+      }
     }else{
-      $("nav.navbar").removeClass("nav-back2");
-      $("nav.navbar").addClass("nav-back1");
-      $("nav.navbar-light ul.navbar-nav li:not(:last-child) a").addClass("nav-text1");
-      $("nav.navbar-light ul.navbar-nav li:not(:last-child) a").removeClass("nav-text2");
-      $(".navbar-brand img.logo_img1").removeClass("hide");
-      $(".navbar-brand img.logo_img2").addClass("hide");
+      navActive();
     }
 
     let scrollList = [];
